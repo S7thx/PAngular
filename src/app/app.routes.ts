b212c72a4dac09ad67da1333b5f2a9c791e0dc63
S7thx <136721499+S7thx@users.noreply.github.com>
2024-11-04 08:44:22 -0300

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ExerciciosComponent } from './pages/exercicios/exercicios.component';
+import { MeditacaoComponent } from './pages/meditacao/meditacao.component';
+import { DicasComponent } from './pages/dicas/dicas.component';
+import { ContatoComponent } from './pages/contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'exercicios', component: ExerciciosComponent },
+  { path: 'meditacao', component: MeditacaoComponent },
+  { path: 'dicas', component: DicasComponent },
+  { path: 'contato', component: ContatoComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/meditacao/meditacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meditacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './meditacao.component.html',
+  styleUrl: './meditacao.component.css'
+})
+export class MeditacaoComponent {
+
+}
